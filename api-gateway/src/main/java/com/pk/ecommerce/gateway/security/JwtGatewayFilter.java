2570498e4eda317a96e002b32c8e9c2b3639abea
GitWpits <pawan@wpitservices.com>
2026-06-08 09:53:24 +0530

    implement GetwayFilter in Gateway service for the authentication/ token verify

--- api-gateway/src/main/java/com/pk/ecommerce/gateway/security/JwtGatewayFilter.java
+++ api-gateway/src/main/java/com/pk/ecommerce/gateway/security/JwtGatewayFilter.java
@@ -1,8 +1,92 @@
 package com.pk.ecommerce.gateway.security;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
 
 @Component
-public class JwtGatewayFilter {
+public class JwtGatewayFilter implements GatewayFilter, Ordered{
 
+	@Autowired
+	private JwtUtil jwtUtil;
+	
+	private static final List<String> Public_URLS= List.of("/api/users/register", "/api/users/login", "/actuator/**");
+	
+	@Override
+	public int getOrder() {
+		
+		return -1;
+	}
+	
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		
+		String path = exchange.getRequest().getURI().getPath();
+		String method = exchange.getRequest().getMethod().name();
+		
+		if(Public_URLS.stream().anyMatch(path::contains)) {
+			return chain.filter(exchange);
+		}
+		
+		String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+		if(authHeader == null || authHeader.startsWith("Bearer ")) {
+			return unauthorized(exchange);
+		}
+		
+		String token = authHeader.substring(7);
+		if(!jwtUtil.validateToken(token)) {
+			return unauthorized(exchange);
+		}
+		
+		String role = jwtUtil.getRole(token);
+		if(!isAuthorized(role, path, method)) {
+			return forbidden(exchange);
+		}
+		
+		String userId = jwtUtil.getUserId(token);
+		
+		
+		ServerHttpRequest modifiedRequest = exchange.getRequest().mutate().header("X-user-Id", userId).header("X-user-role", role).build();
+		return chain.filter(exchange.mutate().request(modifiedRequest).build());
+	}
+	
+	private Mono<Void> unauthorized(ServerWebExchange exchange){
+		exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+		return exchange.getResponse().setComplete();
+	}
+	
+	private static final Map<String, Map<String, List<String>>> ROLE_API_PERMISSION = 
+			Map.of("ADMIN", Map.of(
+			"GET", List.of("/api/products"),
+			"POST", List.of("/api/products"),
+			"PUT", List.of("/api/products"),
+			"DELETE", List.of("/api/products")),
+			"USER",Map.of("GET", List.of("/api/products")));
+	
+	private boolean isAuthorized(String role, String path, String method) {
+		Map<String, List<String>> permission = ROLE_API_PERMISSION.get(role);
+		
+		if(permission == null) return false;
+		
+		List<String> allowedPaths = permission.get(method);
+		if(allowedPaths == null) return false;
+		
+		return allowedPaths.stream().anyMatch(path::startsWith);
+	}
+	
+	private Mono<Void> forbidden(ServerWebExchange exchange) {
+		exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+		return exchange.getResponse().setComplete();
+	}
 }
